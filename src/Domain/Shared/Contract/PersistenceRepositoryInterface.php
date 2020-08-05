e311fc54c09bee6ae75e01c4ed4d5e0145fd335a
victor.fedorenko <victor.fedorenko@mgid.com>
2020-08-06 01:47:51 +0300

    Cleaning the code

--- src/Domain/Shared/Contract/PersistenceRepositoryInterface.php
+++ src/Domain/Shared/Contract/PersistenceRepositoryInterface.php
@@ -7,6 +7,5 @@ namespace App\Domain\Shared\Contract;
 interface PersistenceRepositoryInterface
 {
     public function save(object $object): void;
-
     public function remove(object $object): void;
 }
